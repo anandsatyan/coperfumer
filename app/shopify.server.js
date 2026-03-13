ade9383a9d8931c71b905de53b32095ec90ad8dc
skillza-quest <105557961+skillza-quest@users.noreply.github.com>
2026-03-13 18:56:53 +0530

    Reverting to embedded

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -7,6 +7,9 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("CWD:", process.cwd());
+
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
